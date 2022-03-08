b184159dfaca28c8820443b5b7e61923cfd1d73d
Arita Berisha <a.berisha@commprog.com>
2022-03-08 11:57:09 +0000

    initial commit

---
+++ odoo-15/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'practice-odoo-15',
+    'version': '1.0',
+    'category': 'Practice',
+    'description': """
+                         Test Module
+===========================================================================
+ """,
+    'author': 'Arita',
+    'website': '',
+    'depends': ['base'],
+
+    # always loaded
+    'data': [],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [],
+}
